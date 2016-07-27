b175e0ab8b7a5f9805b857435a33e5e246fee4ee
Henry Fan <henry.fan@centraltrillion.com>
2016-07-27 22:01:30 +0800

    Initialize Project

---
+++ HFSideMenuViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HFSideMenuViewController
+//
+//  Created by HenryFan on 27/7/2016.
+//  Copyright © 2016 HenryFanDi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
