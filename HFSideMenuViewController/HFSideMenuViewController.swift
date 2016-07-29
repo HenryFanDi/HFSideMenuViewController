8b964021ba2091ce075f958ecc1ef41ab9452904
Henry Fan <henry.fan@centraltrillion.com>
2016-07-29 22:27:47 +0800

    feat: add HFSideMenuViewController

---
+++ HFSideMenuViewController/HFSideMenuViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HFSideMenuViewController.swift
+//  HFSideMenuViewController
+//
+//  Created by HenryFan on 29/7/2016.
+//  Copyright © 2016 HenryFanDi. All rights reserved.
+//
+
+import UIKit
+
+class HFSideMenuViewController: UIViewController {
+  
+  // MARK: Lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+}
