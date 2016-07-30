26cfb774272821689c07f5dd64efb56324d42b87
Henry Fan <henry.fan@centraltrillion.com>
2016-07-30 21:23:48 +0800

    feat: add init method to HFSideMenu

--- HFSideMenuViewController/HFSideMenuViewController.swift
+++ HFSideMenuViewController/HFSideMenuViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 
 class HFSideMenuViewController: UIViewController {
   
+  private var menuWidth: CGFloat
+  private var mainViewController: UIViewController
+  private var leftViewController: UIViewController
+  
   // MARK: Lifecycle
+  
+  convenience required init?(coder aDecoder: NSCoder) {
+    self.init(mainViewController: UIViewController(), leftMenuViewController: UIViewController())
+  }
+  
+  init(mainViewController: UIViewController, leftMenuViewController: UIViewController) {
+    self.menuWidth = 0.0
+    self.mainViewController = mainViewController
+    self.leftViewController = leftMenuViewController
+    super.init(nibName: nil, bundle: nil)
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
